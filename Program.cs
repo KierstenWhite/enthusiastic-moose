41ab7d5f1b5d529d5fbf320270a0e092f6281464
Kiersten White <kierstenswhite@gmail.com>
2023-05-22 13:25:11 -0400

    C# Exercise Phase 5

--- Program.cs
+++ Program.cs
@@ -1,11 +1,43 @@
 ﻿using System;
 
+
+/* 
+
+    Boolean with the assigned variable name MooseAsks (string? question?) 
+    console.log the question and (Y/N):
+
+*/
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): "); //Print in the console whatever is declared as question and then (Y/N):
+    string answer = Console.ReadLine().ToLower(); // Reads (looks?) the answer as a string and will also read lowercase?
+
+    while (answer != "y" && answer != "n") //If the user does not type y or n, then it just repeats the question
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y") //If the user answers with the letter y, the value of "true" will print in the console
+    {
+        return true;
+    }
+    else
+    {
+        return false; //If the user answers with the letter n, the value of "false" will print in the console
+    }
+}
+
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
 Console.WriteLine();
 // Let the moose speak!
 MooseSays("H I, I'M  E N T H U S I A S T I C !");
 MooseSays("I really am enthusiastic");
+
+// As a question
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
 
 void MooseSays(string message)
 {
